751b8489d26a1f3fa3345e619117472365209446
TLER AL-BISHI <111729973+tlersa@users.noreply.github.com>
2023-06-28 00:25:02 +0300

    Add files via upload

---
+++ Eid Al-Adha/Eid AL-Adha.js
@@ -0,0 +1,6 @@
+const sr = ScrollReveal ({
+    distance: '90px',
+    duration: 3000, })
+
+sr.reveal(`.home-data`, {origin: 'top', delay: 400})
+sr.reveal(`.home-img`, {origin: 'bottom', delay: 600})
